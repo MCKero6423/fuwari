0d3f3670beb78bcb85995242b4ce700693184c4a
MCKero6423 <mckero64@gmail.com>
2025-08-14 09:49:26 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -54,6 +54,7 @@ export const navBarConfig: NavBarConfig = {
 			name: "状态信息",
       url: "https://status.mckero.com", // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
+		},
 	],
 };
 
