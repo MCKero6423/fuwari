fcdf845e608fea94348d255d82305951fdf7fc3a
QIU <Kero64@126.com>
2026-01-21 22:47:49 +0000

    feat: 集成 DaisyUI 库

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme")
+
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,mjs}"],
-  darkMode: "class", // allows toggling dark mode manually
+  darkMode: "class",
   theme: {
     extend: {
       fontFamily: {
@@ -10,5 +11,8 @@ module.exports = {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("daisyui"),
+  ],
 }
